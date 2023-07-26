a5007d350aaf4aece76ed9382cd18177423bee1c
신현기 <shinHyungi@sinhyeongis-MacBook-Pro.local>
2023-07-26 22:57:33 +0900

    [Add] Swagger 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,10 @@ dependencies {
 	// lombok
 	implementation("org.projectlombok:lombok")
 
+	// swagger
+	implementation("io.springfox:springfox-boot-starter:3.0.0")
+	implementation("io.springfox:springfox-swagger-ui:3.0.0")
+
 	// jwt
 	implementation("io.jsonwebtoken:jjwt-api:0.11.2")
 	implementation("io.jsonwebtoken:jjwt-impl:0.11.2")
